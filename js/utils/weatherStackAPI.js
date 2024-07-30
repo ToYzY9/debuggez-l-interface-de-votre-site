63df2f7150eef9d53424f6f0e61c9b5a022c08e6
BAbdoul <pad1934@hotmail.fr>
2024-07-30 03:41:36 +0200

    Réponse à la question 4

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,18 +1,20 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = "069a0a7fffff5d9f03d7e6e5d7e0e377";
 
-const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
-.then(res => res.json())
-.catch(err => console.log("Oh no", err))
+const _retrieveWeatherForecastMockedData = () =>
+    fetch("/data/weather-api-mocked-data.json")
+        .then((res) => res.json())
+        .catch((err) => console.log("Oh no", err));
 
-
-const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_ke=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
-    .then(res => res.json())
-    .catch(err => console.log("Oh no", err))
-
+const _retrieveWeatherForecastApiData = (coordinates) =>
+    fetch(
+        `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`
+    )
+        .then((res) => res.json())
+        .catch((err) => console.log("Oh no", err));
 
 const retrieveWeatherForecastData = async (coordinates, isMocked) => {
     if (isMocked) {
-        return await _retrieveWeatherForecastMockedData()
+        return await _retrieveWeatherForecastMockedData();
     }
-    return await _retrieveWeatherForecastApiData(coordinates)
-}
+    return await _retrieveWeatherForecastApiData(coordinates);
+};
